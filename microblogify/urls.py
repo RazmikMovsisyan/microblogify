e8d9f6d27e49c3a16c8333dba5cbbf8afbbef503
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-12 13:08:41 +0200

    Remove Forget Password function

--- microblogify/urls.py
+++ microblogify/urls.py
@@ -17,10 +17,14 @@ Including another URLconf
 from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account import views as allauth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/login/', allauth_views.login, name="account_login"),
+    path('accounts/signup/', allauth_views.signup, name="account_signup"),
+    path('accounts/logout/', allauth_views.logout, name="account_logout"),
     path('', include('blog.urls')),
     # path('', lambda request: redirect('post_list')),
 ]
