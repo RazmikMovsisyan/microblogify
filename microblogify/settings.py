498b3f4bba1911bdf8e1186cd0a04966cc702a0f
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-12 12:58:18 +0200

    Install Bootstrap 5 and Cirspy Forms for Login/Signup styling

--- microblogify/settings.py
+++ microblogify/settings.py
@@ -46,8 +46,11 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'django_extensions',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
-
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
