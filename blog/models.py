8b48366415f6cdbbf1628d7073c9ebf6525fcbe6
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-13 17:52:01 +0200

    Function: Comments on Posts

--- blog/models.py
+++ blog/models.py
@@ -41,3 +41,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
