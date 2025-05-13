ebcffa757abf8a58075d932bdccd86f2b9efe993
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-13 20:11:57 +0200

    Function Add Eventlistener for Enter Key

--- blog/forms.py
+++ blog/forms.py
@@ -6,6 +6,17 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ['content']
         widgets = {
-            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Leave a comment...'}),
+            'content': forms.Textarea(attrs={
+                'rows': 3,
+                'maxlength': '1000',
+                'placeholder': 'Leave a comment...',
+            }),
         }
 
+    
+    # Set Limit on input
+    def clean_content(self):
+        content = self.cleaned_data['content']
+        if len(content) > 1000:
+            raise forms.ValidationError("Comment must be 1000 characters or fewer.")
+        return content
