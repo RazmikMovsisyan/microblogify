eef2c523100f749af7bf45199c1117ef074d3a26
Razmik Movsisyan <razmikmovsisyan@Razmiks-MBP.fritz.box>
2025-05-14 20:13:02 +0200

    Style: Post List View has Order where new posts are on top ordered

--- blog/views.py
+++ blog/views.py
@@ -27,6 +27,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    ordering = ['-created_on']
 
 class PostDetailView(DetailView):
     model = Post
