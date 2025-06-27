4c866c4ea231f5dc285d1b781d0c5b7f28df9e42
RazmikMovsisyan <razmikmovsisyan@icloud.com>
2025-06-27 18:46:15 +0200

    Update settings.py DEBUG TRUE
    Instructed by Code Institute I need to change postgres datbaase url due a new password. after setting this in config vars on heroku I received 500 error message. This step will show me the main issue causing this.

--- microblogify/settings.py
+++ microblogify/settings.py
@@ -41,7 +41,7 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
