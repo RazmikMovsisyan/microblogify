f8433d37b68ebdd7816206e93659a6618b019883
Razmik Movsisyan <razmikmovsisyan@iphonerazmik.fritz.box>
2025-07-20 00:19:27 +0200

    JavaScript JsHint Validation Check

--- blog/static/js/javascript.js
+++ blog/static/js/javascript.js
@@ -1,12 +1,14 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    // send comment with enter key
-    document.querySelectorAll("form textarea[name='content']").forEach(function (textarea) {
-      textarea.addEventListener("keydown", function (e) {
-        if (e.key === "Enter" && !e.shiftKey) {
-          e.preventDefault();
-          const form = textarea.closest("form");
-          if (form) form.submit();
+document.addEventListener("DOMContentLoaded", function () {
+  // send comment with enter key
+  document.querySelectorAll("form textarea[name='content']").forEach(function (textarea) {
+    textarea.addEventListener("keydown", function (e) {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        var form = textarea.closest("form");
+        if (form) {
+          form.submit();
         }
-      });
+      }
     });
   });
+});
